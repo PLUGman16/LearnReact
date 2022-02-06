32bf2beb406c5cc36bc37648e9d01b5149036b07
PLUGman <plugman6@gmail.com>
2022-02-06 23:11:50 +0300

    5.2

--- 5/5.ts
+++ 5/5.ts
@@ -14,3 +14,33 @@ function indetity<T extends Array<number>>(arg: T): T {
 }
 indetity([123])
 indetity(['qwerty'])
+
+
+
+//Встроенные Generic
+
+interface IExample<T> {
+    type: string
+    //T передается так же как и свойтсва в функциях
+    value: T
+    isEmpty: boolean
+}
+
+//Omit выбрасывает ключи из интерфейса
+const omittedObj1: Omit<IExample<string>, 'isEmpty'> = {
+    type: 'qwerty',
+    //Ошибка на value тк в типах указали string
+    value: 12345
+}
+const omittedObj2: Omit<IExample<string>, 'value' | 'isEmpty'> = {
+    type: 'qwe',
+    isEmpty: true
+}
+
+//Pick Оставляет только указанный ключи в интерфейсе
+const picked1: Pick<IExample<number>, 'isEmpty'> = {
+    isEmpty: true
+} 
+const picked2: Pick<IExample<number>, 'isEmpty'> = {
+    value: 1234
+} 
