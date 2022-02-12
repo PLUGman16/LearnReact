e3e736f09ba5666184fe71aeafaa6d1e24e7cac8
PLUGman <plugman6@gmail.com>
2022-02-13 00:05:03 +0300

    add folber 4

---
+++ 4/src/Header.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export function Header() {
+    return (
+        <header>
+            <div>Hello world</div>
+            <p>
+                lorem
+            </p>
+        </header>
+    )
+}
